163dd65745e9a872a68f147ff952e02d040a9c60
apps2719 <83412353+apps2719@users.noreply.github.com>
2024-03-13 06:15:26 +0000

    Variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountID = 144553
+let accountEmail = "appspatel774@gmail.com"
+var accountPassword = "12345"
+accountCity = "vadodara"
+
+
+// accountID = 2 // not allowed bcs const value will not change
+
+accountEmail = "apps@gmail.com"
+accountPassword = " 21212121"
+accountCity = " Bangaluru"
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
