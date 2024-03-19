c683f6335a6d574181ba84662a5aace2e358bc9f
apps2719 <83412353+apps2719@users.noreply.github.com>
2024-03-19 06:28:58 +0000

    Datatypes basics done

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //It can treat the whole code as newer version of Java Script code only.
+
+//alert( 3+ 3)
+
+console.log(3 + 
+    3); //code readinility is very high so plase write code properly.
+console.log("Apurva");
