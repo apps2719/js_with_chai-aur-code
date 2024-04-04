25c47ff6bc10c5f31ff4f27f44c778d3c79116a8
apps2719 <83412353+apps2719@users.noreply.github.com>
2024-04-04 13:23:23 +0000

    Datatypes Done

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -5,3 +5,22 @@
 console.log(3 + 
     3); //code readinility is very high so plase write code properly.
 console.log("Apurva");
+
+let name = "Apurva"
+let age = "24"
+let isLoggedIn = false
+
+// Number => 2 Power 53
+//bigint  => for very big number 
+//string  => preffered in ""
+//boolean => true / false
+//null    => stand alon value type
+//undefined => when declare any veriable and cant defne any values to it.
+//symbol  => to find components uniqueness 
+
+
+
+//object  => 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
