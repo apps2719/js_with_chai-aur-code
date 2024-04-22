3d68db479ae6a78c8d3530afe6b16851e3c7a041
apps2719 <83412353+apps2719@users.noreply.github.com>
2024-04-22 06:14:52 +0000

    conversion Done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="33ab"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+//"33" ==> 33
+//"33abc" ==> NaN (Not a Number)
+// true => 1; false => 0
+// "Apps" ==> NaN
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "1" ==> true; "0" ==> false
+//" " ==> false
+//"Apps" ==> true
+
+let sumNumber = 33
+
+let stringNumber = String(sumNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
